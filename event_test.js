cd04e3f82ec8b88a54faaeb36f76aa9e6e36f474
changfish <k0905920491@gmail.com>
2023-01-26 22:44:30 +0800

    new web

--- event_test.js
+++
@@ -1,8 +0,0 @@
-function clubsavings() {
-  var pv = parseInt(document.getElementById("amount").value);
-  var i = parseInt(document.getElementById("rate").value) / 100;
-  var n = parseInt(document.getElementById("period").value);
-  var fvifa = ((1 + i / 12) * Math.pow(1 + i / 12, n) - 1) / (i / 12);
-  var fv = fvifa * pv;
-  document.getElementById("result").innerHTML = "本利和=$" + fv.toFixed(2);
-}
